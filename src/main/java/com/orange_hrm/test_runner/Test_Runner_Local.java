79064672b588c5e9e3fdd960b65e7eb7e4765a4c
afsanakeya <afsanakeya@localhost>
2020-10-04 20:47:53 -0400

    new update

--- src/main/java/com/orange_hrm/test_runner/Test_Runner_Local.java
+++ src/main/java/com/orange_hrm/test_runner/Test_Runner_Local.java
@@ -5,7 +5,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 		plugin= {"pretty","html:target","com.cucumber.listener.ExtentCucumberFormatter:target/Cucumber_Extents_report.html"},
-		features="Feature_Files/Orange_Hrm_LogIn.Feature",
+		features="src/test/resources/Feature_Files/Orange_Hrm_LogIn.feature",
 		glue="com.orange_hrm_step_def"
 		
 		
